0ba94d54fd266e5bec9188d3d071cd4a2650f971
yanakorobova <yanaheaven@yandex.ru>
2023-03-08 20:26:24 +0300

    feat: transfer into redux-toolkit

--- src/state/store.ts
+++ src/state/store.ts
@@ -6,15 +6,19 @@ import thunk, {ThunkDispatch} from "redux-thunk";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {appReducer} from "./app-reducer";
 import {authReducer} from "./auth-reducer";
+import {configureStore} from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer,
     app: appReducer,
-    auth:authReducer
+    auth: authReducer
 })
 
-export const store = createStore(rootReducer, applyMiddleware(thunk))
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunk)
+})
 
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
@@ -22,6 +26,7 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 export type AppThunkDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>
 export const useAppDispatch = () => useDispatch<AppThunkDispatch>()
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+
 
 // @ts-ignore
 window.store = store
