0a24063f748dad3f492b134c010b9c6f042f9063
yanakorobova <08mickey01@gmail.com>
2023-04-10 15:56:25 +0300

    ref: cleanup code

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,6 +1,6 @@
 import {Dispatch} from 'redux'
 import {setAppErrorAC, setAppStatusAC} from "app/app-reducer";
-import {ResponseType} from "../api/todolist-api";
+import {ResponseType} from "api/types";
 // generic function
 export const handleServerAppError = <T>(data: ResponseType<T>, dispatch: Dispatch) => {
     data.messages.length ? dispatch(setAppErrorAC({error: data.messages[0]}))
